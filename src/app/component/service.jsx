ab1c7123b8fc342a296ede0d4c98aa3bc8319c65
PENDOL10 <fendyrarestu2004@gmail.com>
2025-08-06 22:13:37 +0700

    Update Section Service

--- src/app/component/service.jsx
+++ src/app/component/service.jsx
@@ -3,6 +3,9 @@
 import React, { forwardRef, useState } from "react";
 import { motion } from "framer-motion";
 import { Montserrat, Space_Mono } from "next/font/google";
+import { HiOutlineSparkles } from "react-icons/hi2";
+import { FaCode } from "react-icons/fa6";
+import { TbHandClick } from "react-icons/tb";
 import Link from "next/link";
 
 const montserrat = Montserrat({
@@ -57,11 +60,70 @@ const Service = forwardRef((props, ref) => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.6 }}
-            className="text-4xl md:text-5xl lg:text-6xl font-bold justify-left mb-14 px-2 sm:px-4 lg:px-6 text-gray-900 dark:text-white"
+            className="text-4xl md:text-5xl lg:text-6xl font-bold justify-left mb-16 px-2 sm:px-4 lg:px-6 text-gray-900 dark:text-white"
           >
             The Essentials:
           </motion.div>
 
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {/* Service Items 1*/}
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: 0.4 }}
+              viewport={{ once: true }}
+              className="p-6 flex flex-col items-start hover:shadow-lg transition-shadow duration-300 mt-16"
+            >
+              <HiOutlineSparkles size={90} className="text-foreground" />
+              <h2 className="text-3xl md:text-54xl lg:text-5xl mb-6 font-bold">
+                Ideation & Wireframing
+              </h2>
+              <p className="text-gray-600 dark:text-gray-400 text-justify">
+                I transform ideas into clear, user-centered wireframes that
+                focus on usability and structure. This stage lays the foundation
+                for seamless UI/UX by aligning visual logic with user goals.
+              </p>
+            </motion.div>
+
+            {/* Service Items 2*/}
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: 0.4 }}
+              viewport={{ once: true }}
+              className="p-6 flex flex-col items-start hover:shadow-lg transition-shadow duration-300 mt-16"
+            >
+              <FaCode size={90} className="text-foreground" />
+              <h2 className="text-3xl md:text-54xl lg:text-5xl mb-6 font-bold">
+                Design to Code
+              </h2>
+              <p className="text-gray-600 dark:text-gray-400 text-justify">
+                I turn high-fidelity designs into responsive, maintainable
+                frontend code using modern frameworks. Every element is built
+                with precision to ensure both performance and visual
+                consistency.
+              </p>
+            </motion.div>
+
+            {/* Service Items 3*/}
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: 0.4 }}
+              viewport={{ once: true }}
+              className="p-6 flex flex-col items-start hover:shadow-lg transition-shadow duration-300 mt-16"
+            >
+              <TbHandClick size={90} className="text-foreground" />
+              <h2 className="text-3xl md:text-54xl lg:text-5xl mb-6 font-bold">
+                Interaction Development
+              </h2>
+              <p className="text-gray-600 dark:text-gray-400 text-justify">
+                I enhance user experience through purposeful animations and
+                micro-interactions. From subtle hover effects to dynamic
+                transitions, every movement adds clarity, emotion, and delight.
+              </p>
+            </motion.div>
+          </div>
         </div>
       </motion.section>
     );
